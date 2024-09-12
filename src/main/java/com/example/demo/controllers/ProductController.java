68a16578d45b3fc5f8a75fabf294285fe6e757f4
kunxin-chor <chorkunxin@yahoo.com>
2024-09-12 09:05:13 +0000

    Added ability to list products

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.models.Product;
 import com.example.demo.repo.ProductRepo;
@@ -19,6 +20,7 @@ public class ProductController {
         this.productRepo = productRepo;
     }
     
+    @GetMapping("/products")
     public String listProducts(Model model) {
         List<Product> products = productRepo.findAll();
         model.addAttribute("products", products);

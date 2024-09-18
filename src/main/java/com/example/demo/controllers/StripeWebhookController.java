abc1acd1273fd42bf2f0f138f8d68ff668c6d47f
kunxin-chor <chorkunxin@yahoo.com>
2024-09-18 02:42:23 +0000

    Incorporate order service into webhook controller

--- src/main/java/com/example/demo/controllers/StripeWebhookController.java
+++ src/main/java/com/example/demo/controllers/StripeWebhookController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.services.OrderService;
 import com.stripe.exception.SignatureVerificationException;
 import com.stripe.model.Event;
 import com.stripe.net.Webhook;
@@ -22,9 +24,12 @@ public class StripeWebhookController {
     
     @Value("${stripe.webhook.secret}")
     private String endpointSecret;
+    
+    private final OrderService orderService;
 
-    public StripeWebhookController() {
-
+    @Autowired
+    public StripeWebhookController(OrderService orderService) {
+        this.orderService = orderService;
     }
 
     // Stripe-Signature: whenever Stripe calls our endpoint (aka route),
@@ -45,6 +50,10 @@ public class StripeWebhookController {
         }
 
         System.out.println(event);
+
+        if (event.getType().equals("checkout.session.completed")) {
+            orderService.handleSuccessfulPayment(event);
+        }
 
         return ResponseEntity.ok().build();  // send back HTTP Status Code 200
 
